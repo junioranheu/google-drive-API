928d2cfdafffffb7169d0784443c89df1bf53996
junioranheu <junioranheu@gmail.com>
2023-11-22 16:30:29 -0300

    criar camada DriveAnheu.Tests & implementar UsuarioUseCaseTest

---
+++ src/DriveAnheu.Tests/Fixtures/Fixture.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using DriveAnheu.Application.AutoMapper;
+using DriveAnheu.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace DriveAnheu.Tests.Fixtures
+{
+    public static class Fixture
+    {
+        public static DriveAnheuContext CriarContext()
+        {
+            DbContextOptions<DriveAnheuContext> mock = new DbContextOptionsBuilder<DriveAnheuContext>().UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()).Options;
+            DriveAnheuContext? context = new(mock);
+
+            return context;
+        }
+
+        public static IMapper CriarMapper()
+        {
+            MapperConfiguration mockMapper = new(x =>
+            {
+                x.AddProfile(new AutoMapperConfig());
+            });
+
+            IMapper map = mockMapper.CreateMapper();
+
+            return map;
+        }
+    }
+}
---
+++ src/DriveAnheu.Tests/Tests/Usuarios/UsuarioUseCaseTest.cs
@@ -0,0 +1,43 @@
+﻿using DriveAnheu.Application.UseCases.Usuarios.AutenticarUsuario;
+using DriveAnheu.Application.UseCases.Usuarios.CriarUsuario;
+using DriveAnheu.Application.UseCases.Usuarios.Shared.Output;
+using DriveAnheu.Domain.Entities;
+using DriveAnheu.Infrastructure.Auth.Token;
+using Moq;
+using Xunit;
+
+namespace DriveAnheu.Tests.Tests.Usuarios
+{
+    public sealed class UsuarioUseCaseTest
+    {
+        [Fact]
+        public async Task Autenticar_ChecarResultadoEsperado()
+        {
+            // Arrange
+            var criarUsuarioCommandMock = new Mock<ICriarUsuarioCommand>();
+            var jwtTokenGeneratorMock = new Mock<IJwtTokenGenerator>();
+
+            UsuarioOutput input = new()
+            {
+                UsuarioId = 1,
+                Guid = Guid.NewGuid()
+            };
+
+            criarUsuarioCommandMock.Setup(x => x.Execute(It.IsAny<Usuario>())).ReturnsAsync(input);
+
+            string token = Guid.NewGuid().ToString();
+            jwtTokenGeneratorMock.Setup(x => x.GerarToken(It.IsAny<int>(), It.IsAny<Guid>())).Returns(token);
+
+            var service = new AutenticarUsuarioCommand(jwtTokenGeneratorMock.Object, criarUsuarioCommandMock.Object);
+
+            // Act
+            var result = await service.Execute();
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(input.UsuarioId, result.UsuarioId);
+            Assert.Equal(input.Guid, result.Guid);
+            Assert.Equal(token, result.Token);
+        }
+    }
+}
